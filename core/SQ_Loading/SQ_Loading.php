1fc5adda748a84a9f4bc4f864472ab661da10c28
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-21 08:59:26 +0000

    update languages
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@656151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/SQ_Loading/SQ_Loading.php
+++ core/SQ_Loading/SQ_Loading.php
@@ -30,6 +30,8 @@ class SQ_Loading extends SQ_BlockController {
         }else {
             echo '<script type="text/javascript">
                     var sq_uri = "'.SQ_URI.'"; var sq_language = "'.get_bloginfo('language').'"; var sq_version = "'.SQ_VERSION_ID.'"; var sq_wpversion = "'.WP_VERSION_ID.'"; var sq_phpversion = "'.PHP_VERSION_ID.'"; var __postID = "'.$post_ID.'"; var __token = "'.SQ_Tools::$options['sq_api'].'";
+                    var __noopt = "'.__('You haven`t used Squirrly SEO to optimize your article. Do you want to optimize for a keyword before publishing?',_PLUGIN_NAME_).'";
+                        
                     (function() {this.sq_tinymce = { callback: function () {}, setup: function(ed){} } })(window);
                   </script>';
 
