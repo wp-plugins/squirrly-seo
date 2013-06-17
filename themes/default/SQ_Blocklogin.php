c459a091da747c714f769490acabb18436c7f86b
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-17 10:35:32 +0000

    fixed Block Login SignUp
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@727355 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/SQ_Blocklogin.php
+++ themes/default/SQ_Blocklogin.php
@@ -9,7 +9,7 @@
             <li><label for="sq_user"><?php _e('Email:', _PLUGIN_NAME_); ?></label><input type="text" id="sq_user" name="sq_user"  autocomplete="off" /></li>
             <li><label for="sq_password"><?php _e('Password:', _PLUGIN_NAME_); ?></label><input type="password" id="sq_password" name="sq_password" autocomplete="off" /></li>
             <li><input type="button" id="sq_login" value="<?php _e('Login', _PLUGIN_NAME_); ?>"  /></li>
-            <li><a href="<?php echo _SQ_DASH_URL_ .'login/?action=register'?>" target="_blank" title="<?php _e('Register', _PLUGIN_NAME_); ?>"><?php _e('Register to Squirrly.co', _PLUGIN_NAME_); ?></a> |
+            <li><a id="sq_signup" href="javascript:void(0);" target="_blank" title="<?php _e('Register', _PLUGIN_NAME_); ?>"><?php _e('Register to Squirrly.co', _PLUGIN_NAME_); ?></a> |
                 <a href="<?php echo _SQ_DASH_URL_ .'login/?action=lostpassword'?>" target="_blank" title="<?php _e('Lost password?', _PLUGIN_NAME_); ?>"><?php _e('Lost password', _PLUGIN_NAME_); ?></a></li>
           </ul>
           <div id="sq_autologin" align="center">
