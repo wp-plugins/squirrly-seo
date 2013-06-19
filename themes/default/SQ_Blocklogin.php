f7437582ce6df57dfba02e4a82a3578f344e79c6
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-19 14:28:36 +0000

    added signin information
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@728393 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/SQ_Blocklogin.php
+++ themes/default/SQ_Blocklogin.php
@@ -6,8 +6,8 @@
                 <div class="sq_error"></div>
                 <div class="sq_message" style="display: none;"></div>
             </li>
-            <li><label for="sq_user"><?php _e('Email:', _PLUGIN_NAME_); ?></label><input type="text" id="sq_user" name="sq_user"  autocomplete="off" /></li>
-            <li><label for="sq_password"><?php _e('Password:', _PLUGIN_NAME_); ?></label><input type="password" id="sq_password" name="sq_password" autocomplete="off" /></li>
+            <li><label for="sq_user"><?php _e('Email:', _PLUGIN_NAME_); ?></label><input type="text" id="sq_user" name="sq_user" autocomplete="on" /></li>
+            <li><label for="sq_password"><?php _e('Password:', _PLUGIN_NAME_); ?></label><input type="password" id="sq_password" name="sq_password"  /></li>
             <li><input type="button" id="sq_login" value="<?php _e('Login', _PLUGIN_NAME_); ?>"  /></li>
             <li><a id="sq_signup" href="javascript:void(0);" target="_blank" title="<?php _e('Register', _PLUGIN_NAME_); ?>"><?php _e('Register to Squirrly.co', _PLUGIN_NAME_); ?></a> |
                 <a href="<?php echo _SQ_DASH_URL_ .'login/?action=lostpassword'?>" target="_blank" title="<?php _e('Lost password?', _PLUGIN_NAME_); ?>"><?php _e('Lost password', _PLUGIN_NAME_); ?></a></li>
@@ -15,7 +15,7 @@
           <div id="sq_autologin" align="center">
               <div class="sq_error"></div>
               <span id="sq_register"><?php _e('Enter your email', _PLUGIN_NAME_); ?></span><span id="sq_register_wait"></span>
-              <div id="sq_register_email" ><label for="sq_email"><?php _e('Your E-mail:', _PLUGIN_NAME_); ?></label><input type="text" id="sq_email" name="sq_email" autocomplete="off" /></div>
+              <div id="sq_register_email" ><label for="sq_email"><?php _e('Your E-mail:', _PLUGIN_NAME_); ?></label><input type="text" id="sq_email" name="sq_email" autocomplete="on" /></div>
               <div id="sq_loginimage"><?php _e('Sign Up', _PLUGIN_NAME_); ?></div>
               <div id="sq_signin"><?php _e('I already have an account', _PLUGIN_NAME_); ?></div>
               <span><?php _e('This email connects you to Squirrly.co' , _PLUGIN_NAME_); ?></span>
@@ -35,4 +35,15 @@
               sq_autoLogin();
           });
       </script>
+</div>
+<div id="sq_login_success" style="display: none;">
+    <div class="sq_header"><?php _e('Congratulation! Write a new post with:', _PLUGIN_NAME_); ?></div>
+    <img src="<?php echo _SQ_STATIC_API_URL_?>default/img/squirrly_wordpress.png" />
+    <ul>
+        <li><?php _e('<strong>Keyword Research</strong>: find the perfect keywords for your articles.', _PLUGIN_NAME_); ?></li>
+        <li><?php _e('<strong>Inspiration box</strong>: pulls images, tweets, wikis and blogs, so that you can inspire yourself very fast on what to write in your article.', _PLUGIN_NAME_); ?></li>
+        <li><?php _e('<strong>SEO Live Assistant</strong>: Checks your SEO in real time. Before you click the Post button, you know if your article is perfectly optimized or not.', _PLUGIN_NAME_); ?></li>
+        <li><?php _e('<strong>Article Rank</strong>: A great tool to track your progress on each article. You get Traffic, Social, Inbound-Links and Search engines Rank information instantly.', _PLUGIN_NAME_); ?></li>
+    </ul>
+
 </div>
