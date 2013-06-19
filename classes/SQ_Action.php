0d26d4ac022a0b27e7a5595689f4de8e04f7c5da
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-19 10:39:05 +0000

    fixed bugs for not public posts
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@728312 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/SQ_Action.php
+++ classes/SQ_Action.php
@@ -147,7 +147,6 @@ class SQ_Action extends SQ_FrontController{
            $parameters = 'q='.base64_encode($parameters);
 
        $url = self::cleanUrl(_SQ_API_URL_.$module."?".$parameters);
-
        SQ_Tools::dump($url);
 
        return SQ_Tools::sq_remote_get($url,array('timeout' => 60));
