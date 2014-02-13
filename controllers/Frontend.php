6a8b9b49d6cec7bc8d6a54b417d593d95ba4e919
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 12:14:43 +0000

    update js loading
    git-svn-id: https://plugins.svn.wordpress.org/starbox/trunk@857163 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- controllers/Frontend.php
+++ controllers/Frontend.php
@@ -70,6 +70,9 @@ class ABH_Controllers_Frontend extends ABH_Classes_FrontController {
             if (file_exists(_ABH_ALL_THEMES_DIR_ . $theme . '/css/frontend.css'))
                 ABH_Classes_ObjController::getController('ABH_Classes_DisplayController')
                         ->loadMedia(_ABH_ALL_THEMES_URL_ . $theme . '/css/frontend.css'); //load the css and js for frontend
+            if (file_exists(_ABH_ALL_THEMES_DIR_ . $theme . '/js/frontend.js'))
+                ABH_Classes_ObjController::getController('ABH_Classes_DisplayController')
+                        ->loadMedia(_ABH_ALL_THEMES_URL_ . $theme . '/js/frontend.js'); //load the css and js for frontend
         }
         //
         //show all the authors in the content
