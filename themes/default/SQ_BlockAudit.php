34514364969f22063e7c8662a7573111fa85cfb2
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-03 17:07:39 +0000

    fixed constant issue
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@868675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/default/SQ_BlockAudit.php
+++ themes/default/SQ_BlockAudit.php
@@ -1,15 +1,15 @@
 <div id="sq_option_audit">
     <span id="sq_option_audit_link" style="display: none">
-        <?php echo '<a class="sq_button"  target="_blank">' . __('Check your site audit', _sq_PLUGIN_NAME_) . '</a>' ?>
+        <?php echo '<a class="sq_button"  target="_blank">' . __('Check your site audit', _PLUGIN_NAME_) . '</a>' ?>
     </span>
     <span id="sq_option_audit_notmade" style="display: none">
-        <?php _e('The audit will be ready in 1-3 days ...', _sq_PLUGIN_NAME_) ?>
+        <?php _e('The audit will be ready in 1-3 days ...', _PLUGIN_NAME_) ?>
     </span>
     <span id="sq_option_audit_notready" style="display: none">
-        <?php _e('The audit will be ready soon ...', _sq_PLUGIN_NAME_) ?>
+        <?php _e('The audit will be ready soon ...', _PLUGIN_NAME_) ?>
     </span>
     <span id="sq_option_audit_error" style="display: none">
-        <?php _e('Squirrly receives an error page while checking your site ...', _sq_PLUGIN_NAME_) ?>
+        <?php _e('Squirrly receives an error page while checking your site ...', _PLUGIN_NAME_) ?>
     </span>
 </div>
 <script type="text/javascript">
