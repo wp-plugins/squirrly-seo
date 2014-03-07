e78a2e628f68d251a9c3008e86ee19baf6d327dd
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-07 06:26:13 +0000

    update the traffic recorder
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@871048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- controllers/SQ_Frontend.php
+++ controllers/SQ_Frontend.php
@@ -120,8 +120,8 @@ class SQ_Frontend extends SQ_FrontController {
             //Use buffer only for meta Title
             //if(self::$options['sq_auto_title'] == 1)
             $this->model->flushHeader();
-            $this->model->recordTraffic();
         }
+        $this->model->recordTraffic();
     }
 
 }
