dc68b170ccfff624f89a457de160c8f2d64e0744
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 08:28:07 +0000

    Fixed FollowLocation when safe mode is on or open_basedir
    git-svn-id: https://plugins.svn.wordpress.org/squirrly-seo/trunk@1147849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/SQ_Tools.php
+++ classes/SQ_Tools.php
@@ -339,7 +339,7 @@ class SQ_Tools extends SQ_FrontController {
         }
         $options['sslverify'] = false;
 
-        if (function_exists('curl_init')) {
+        if (function_exists('curl_init') && !ini_get('safe_mode') && !ini_get('open_basedir')) {
             return self::sq_curl($url, $options);
         } else {
             return self::sq_wpcall($url, $options);
@@ -364,7 +364,7 @@ class SQ_Tools extends SQ_FrontController {
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_TIMEOUT, $options['timeout']);
 
-        if (isset($options['followlocation']) && !ini_get('safe_mode')) {
+        if (isset($options['followlocation']) ) {
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
         }
